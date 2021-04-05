5fef0308621a858eb04d0d71717d5a3a0c4d657a
itsaimal <itsaimalyousafzai@gmail.com>
2021-04-05 17:12:12 +0500

    carticon,dropdown,toggle added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,9 +4,11 @@ import {Link} from "react-router-dom"
 import {ReactComponent as Logo} from "../../assets/crown.svg"
 import {auth} from "../../firebase/firebase"
 import {connect} from "react-redux"
+import CartIcon from "../cart-icon/CartIcon"
+import CartDropdown from "../cart/CartDropdown"
 
 
-function Header({currentUser}) {
+function Header({currentUser,hidden}) {
     return (
         <div className="header">
 <Link className="logo-container" to="/">
@@ -20,14 +22,18 @@ function Header({currentUser}) {
 
     { currentUser ?( <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div> ): (<Link className="option" to="/signin">SIGN IN</Link>)}
 
-</div>
+    <CartIcon/>
+
+</div>{ hidden ? null :
+<CartDropdown/>}
             
         </div>
     )
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user:{currentUser}, cart:{hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
